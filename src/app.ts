ca22e0ed15bdb0704fdafcd7ec0c8e27fd0e181b
Dawid Kostrzewa <kostrzewadawid1@gmail.com>
2020-07-08 20:28:09 +0200

    init

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+// Controllers (route handlers)
+import * as homeController from "./controllers/home";
+import * as apiController from "./controllers/api";
+
+// Create Express server
+const app = express();
+
+// Express configuration
+app.set("port", process.env.PORT || 8080);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+/**
+ * Primary app routes.
+ */
+app.get("/", homeController.index);
+
+/**
+ * API examples routes.
+ */
+app.get("/api", apiController.getApi);
+
+export default app;
